47cd0e85f9492d7923d7f8c70b447c2af8ff9ee6
Monica Williams <monicaestellaw@gmail.com>
2017-04-05 16:18:45 -0700

    all tests are passing

--- exercism/anagram/anagram.js
+++ exercism/anagram/anagram.js
@@ -4,19 +4,16 @@ function Anagram( str ) {
 }
 
 Anagram.prototype.matches = function( ...input ) {
-  // loop through all chars in str and mark # of time each letter apears
+  if( typeof input[0] === 'object' ) {
+    input = input[0]
+  }
   let foundAnagrams = []
   this.hashLetterCount()
 
-  // loop through all of the words we are checcking to see if they
-  // are anagrams of this.str
-  if ( input[0] instanceof Array ) {
-    
-    for( let word of input ) {
-      console.log('word', word)
-      if( this.isAnagram( word )) {
-        foundAnagrams.push(word)
-      }
+  for( let word of input ) {
+    console.log('word', word)
+    if( this.isAnagram( word )) {
+      foundAnagrams.push(word)
     }
   }
 
@@ -55,13 +52,6 @@ Anagram.prototype.isAnagram = function( word ) {
     }
   }
   return true
-  //loop through letterHash.
-  //if there are any values that are still greater than 0, return false
-  //word = the word at any index of the array
-  //check each of those words against this.str
 }
-
-let myAnagram = new Anagram( 'hello' )
-myAnagram.matches( ['olleh', 'goodbye'])
 
 module.exports = Anagram
