a706fc1b1f832ea367a4871d4323bc7c84ad5e23
Monica Williams <monicaestellaw@gmail.com>
2017-04-06 14:56:21 -0700

    changing branches

--- exercism/robot-name/robot-name.js
+++ exercism/robot-name/robot-name.js
@@ -1,6 +1,8 @@
 function Robot() {
   this.letterMap = {
-    1: a, 2: b, 3: c,
+    1: 'A', 2: 'B', 3: C, 4: D, 5: E, 6: F, 7: G, 8: H, 9: I, 10: J, 11: K, 12: L,
+    13: M, 14: N, 15: O, 16: P, 17: Q, 18: R, 19: S, 20: T, 21: U, 22: V, 23: W,
+    24: X, 25: Y, 26: Z
   }
   this.name
 }
@@ -21,6 +23,10 @@ Robot.prototype.generateRandomLetters() {
 }
 
 Robot.prototype.generateRandomNumbers() {
+  if ( Math.floor( Math.random() * 1000 ) < 100 ) {
+    return Math.floor( Math.random() * 1000 ) * 10
+  } else {
   return Math.floor( Math.random() * 1000 )
-  //sometimes numbers come back as two digits
+  }
 }
+//sometimes numbers come back as two digits
