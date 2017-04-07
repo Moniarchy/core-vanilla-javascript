66b122bac94b9717b02831c1f2e495fedf8b8128
Monica Williams <monicaestellaw@gmail.com>
2017-04-07 11:28:26 -0700

    creates start implementation for shift

--- exercism/linked-list/linked-list.js
+++ exercism/linked-list/linked-list.js
@@ -39,7 +39,24 @@ if ( this.length > 1 ) {
 }
 
 LinkedList.prototype.shift = function() {
+  var nodeToBeShifted
+  var temp = this.top
 
+  while ( temp.prev ) {
+    temp = temp.prev
+
+    if ( temp.prev === null ) {
+      nodeToBeShifted = temp.data
+      console.log('temp', nodeToBeShifted)
+      // temp = temp.next
+      this.length--
+      return nodeToBeShifted
+    }
+    }
+  //start at top
+  //go through list and access prev until we find the node where prev === null
+  //set that node's next to have a prev of null
+  //return that node that was removed
 }
 
 LinkedList.prototype.unshift = function() {
@@ -54,4 +71,9 @@ module.exports = LinkedList
 
 var chain = new LinkedList
 
-console.log(chain.push(5))
+chain.push(6)
+chain.push(9)
+chain.push(12)
+chain.push(15)
+
+console.log( chain.shift())
